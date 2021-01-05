9760141cbb41e73d08edb2e85056b74461ed5975
Mirzagayev Aydin <am180@hdm-stuttgart.de>
2021-01-05 14:55:48 +0100

    offset hinzugefügt

--- Klausuraufgabe1/Aufgabe.cpp
+++ Klausuraufgabe1/Aufgabe.cpp
@@ -1,5 +1,20 @@
-// Klausuraufgabe1.cpp : Diese Datei enthält die Funktion "main".
-// Hier beginnt und endet die Ausführung des Programms.
+/**********************************************************************
+Copyright ©2013 Advanced Micro Devices, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+•	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+•	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************/
+
+// For clarity,error checking has been omitted.
 
 #include <CL/cl.h>
 #include <string.h>
@@ -9,86 +24,54 @@
 #include <string>
 #include <fstream>
 
-#include "OpenCLMgr.h"
-
 #define SUCCESS 0
 #define FAILURE 1
 
 using namespace std;
 
-cl_int praefixsummeRek(cl_mem aBuffer, cl_int* output, int size, OpenCLMgr& mgr)
+#include "OpenCLMgr.h"
+
+// size of arrays must be exactly 256
+int praefixsumme(cl_int *input, cl_int *output, int size, OpenCLMgr& mgr)
 {
 	cl_int status;
-	int fields = 256;
-	size_t outsize = size / fields;
-	int clsize = (outsize + (fields-1)) / fields * fields;  // das Vielfache von 256 (256,512,1024,1280,...)
 
-	cout << "\nsize: " << size << "\n";
-	cout << "outsize: " << outsize << "\n";
-	cout << "clsize: " << clsize << "\n";
+	int clsize = (size + 8) / 8 * 8;  // next multiple of 512
+
+	// create OpenClinput buffer
+	cl_mem inputBuffer = clCreateBuffer(mgr.context, CL_MEM_READ_ONLY, clsize * sizeof(cl_int),NULL, NULL);
+	status = clEnqueueWriteBuffer(mgr.commandQueue, inputBuffer, CL_TRUE, 0, clsize * sizeof(cl_int), input, 0, NULL, NULL);
+	CHECK_SUCCESS("Error: writing buffer!")
+	if (size < clsize) {
+		// unintialisierte Elemente [0-255] 0 setzen, um Berechnung zu ermöglichen
+		cl_int tmp[8] = { 0 };
+		status = clEnqueueWriteBuffer(mgr.commandQueue, inputBuffer, CL_TRUE, size * sizeof(cl_int), (clsize-size)*sizeof(cl_int), &tmp, 0, NULL, NULL);
+		CHECK_SUCCESS("Error: writing buffer!")
+	}
 
 	// create OpenCl buffer for output
-	cl_mem bBuffer = clCreateBuffer(mgr.context, CL_MEM_READ_WRITE, clsize * sizeof(cl_int), NULL, NULL);
+	cl_mem outputBuffer = clCreateBuffer(mgr.context, CL_MEM_READ_WRITE, clsize * sizeof(cl_int), NULL, NULL);
 
 	// Set kernel arguments.
-	status = clSetKernelArg(mgr.praefixsumme256_kernel, 0, sizeof(cl_mem), (void*)&aBuffer);
+	status = clSetKernelArg(mgr.praefixsumme256_kernel, 0, sizeof(cl_mem), (void *)&inputBuffer);
 	CHECK_SUCCESS("Error: setting kernel argument 1!")
-	status = clSetKernelArg(mgr.praefixsumme256_kernel, 1, sizeof(cl_mem), (void*)&bBuffer);
+	status = clSetKernelArg(mgr.praefixsumme256_kernel, 1, sizeof(cl_mem), (void *)&outputBuffer);
 	CHECK_SUCCESS("Error: setting kernel argument 2!")
 
 	// Run the kernel.
-	size_t global_work_size[1] = { fields };
-	size_t local_work_size[1] = { fields };
+	size_t global_work_size[1] = { clsize };
+	size_t local_work_size[1] = { clsize };
 	status = clEnqueueNDRangeKernel(mgr.commandQueue, mgr.praefixsumme256_kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, NULL);
 	CHECK_SUCCESS("Error: enqueuing kernel!")
 
 	// get resulting array
-	status = clEnqueueReadBuffer(mgr.commandQueue, bBuffer, CL_TRUE, 0, clsize * sizeof(cl_int), output, 0, NULL, NULL);
-	CHECK_SUCCESS("Error: reading bBuffer!")
-
-	if (outsize > 1) {
-		if (size < clsize) {
-			// unintialisierte Elemente [0-255] 0 setzen, um Berechnung zu ermöglichen
-			cl_int tmp[256] = { 0 };
-			status = clEnqueueWriteBuffer(mgr.commandQueue, aBuffer, CL_TRUE, size * sizeof(cl_int), (clsize-size)*sizeof(cl_int), &tmp, 0, NULL, NULL);
-			CHECK_SUCCESS("Error: writing buffer!")
-			cout << "aBuffer override #58\n";
-		}
-
-		praefixsummeRek(aBuffer, output, clsize, mgr);
-		cout << "more then 256 elements" << "\n";
-	}
-	
-	status = clReleaseMemObject(bBuffer);
-	CHECK_SUCCESS("Error: releasing buffer!")
-}
-
-// size of arrays must be exactly 256
-cl_int praefixsumme(cl_int* input, cl_int* output, int size, OpenCLMgr& mgr)
-{
-	cl_int status;
-	int fields = 256;
-	int clsize = (size + (fields-1)) / fields * fields;  // das Vielfache von 256 (256,512,1024,1280,...)
-	
-	cout << "\nsize: " << size << "\n";
-	cout << "clsize: " << clsize << "\n";
-
-	// create OpenClinput buffer
-	cl_mem aBuffer = clCreateBuffer(mgr.context, CL_MEM_READ_ONLY, clsize * sizeof(cl_int),NULL, NULL);
-	status = clEnqueueWriteBuffer(mgr.commandQueue, aBuffer, CL_TRUE, 0, clsize * sizeof(cl_int), input, 0, NULL, NULL);
-	CHECK_SUCCESS("Error: writing aBuffer!")
-	if (size < clsize) {
-		// unintialisierte Elemente [0-255] 0 setzen, um Berechnung zu ermöglichen
-		cl_int tmp[256] = { 0 };
-		status = clEnqueueWriteBuffer(mgr.commandQueue, aBuffer, CL_TRUE, size * sizeof(cl_int), (clsize-size)*sizeof(cl_int), &tmp, 0, NULL, NULL);
-		CHECK_SUCCESS("Error: writing buffer!")
-		cout << "aBuffer override #88\n";
-	}
-
-	praefixsummeRek(aBuffer, output, clsize, mgr);
+	status = clEnqueueReadBuffer(mgr.commandQueue, outputBuffer, CL_TRUE, 0, clsize * sizeof(cl_int), output, 0, NULL, NULL);
+	CHECK_SUCCESS("Error: reading buffer!")
 
 	// release buffers
-	status = clReleaseMemObject(aBuffer);		
+	status = clReleaseMemObject(inputBuffer);		
+	CHECK_SUCCESS("Error: releasing buffer!")
+	status = clReleaseMemObject(outputBuffer);
 	CHECK_SUCCESS("Error: releasing buffer!")
 
 	return SUCCESS;
@@ -99,18 +82,19 @@ int main(int argc, char* argv[])
 	OpenCLMgr mgr;
 
 	// Initial input,output for the host and create memory objects for the kernel
-	int size = 510;
-	cl_int* input = new cl_int[size];
-	cl_int* output = new cl_int[size];
-	for (int i = 0; i < size; i++)
+	int size = 14;
+	cl_int *input = new cl_int[size];
+	cl_int *output = new cl_int[size];
+
+	for (int i=0 ; i<size ; i++)
 		input[i] = 1;
 
+	// call function
 	praefixsumme(input, output, size, mgr);
-
+	
 	delete[] input;
 	delete[] output;
 
-	std::cout << "Passed!\n";
-	
-	return 1;
+	std::cout<<"Passed!\n";
+	return SUCCESS;
 }
--- Klausuraufgabe1/Kernel.cl
+++ Klausuraufgabe1/Kernel.cl
@@ -1,29 +1,21 @@
-/*
-Zentraler Bestandteil jedes OpenCL-Programms ist der Kernel, der von der GPU ausgefuhrt
-wird. In diesem Abschnitt werden die Grundlagen der Kernel-Programmierung in der Programmiersprache
-OpenCL erlautert.
-Die Programmiersprache OpenCL ist von C abgeleitet. Ein OpenCL-Kernel hat daher Ahnlichkeit
-mit einer C-Funktion. Es werden jedoch keine Daten zuruckgegeben, daher handelt
-es sich um eine Funktion mit Ruckgabetyp void, wie an folgendem Beispiel deutlich wird:
-*/
-
 __kernel void praefixsumme256_kernel(__global int* in, __global int* out)
 {
 	int gid = get_global_id(0);
 	int lid = get_local_id(0);
 	int groupid = get_group_id(0);
 	
-	__local int localArray[256];
+	__local int localArray[8];
 	
-	int k = 8;	// depth of tree: log2(256)
+	int k = 3;	// depth of tree: log2(256)
 	int d, i, i1, i2;
 
 	// copy to local memory
 	localArray[lid] = in[gid];
 	barrier(CLK_LOCAL_MEM_FENCE);
 
+	
 	// Up-Sweep
-	int noItemsThatWork = 128;
+	int noItemsThatWork = 4;
 	int offset = 1;
 	for (d = 0; d<k; d++, noItemsThatWork >>= 1, offset <<= 1) {
 		if (lid < noItemsThatWork) {
@@ -34,13 +26,15 @@ __kernel void praefixsumme256_kernel(__global int* in, __global int* out)
 		barrier(CLK_LOCAL_MEM_FENCE);
 	}
 
+
 	// Down-Sweep
-	if (lid == 255)
-		localArray[255] = 0;
+	if (lid == 7)
+		localArray[7] = 0;
 	barrier(CLK_LOCAL_MEM_FENCE);
+	
 
 	noItemsThatWork = 1;
-	offset = 128;
+	offset = 4;
 	for (d = 0; d<k; d++, noItemsThatWork <<= 1, offset >>= 1)
 	{
 		if (lid < noItemsThatWork) {
@@ -53,10 +47,7 @@ __kernel void praefixsumme256_kernel(__global int* in, __global int* out)
 		barrier(CLK_LOCAL_MEM_FENCE);
 	}
 
+
 	// write result to global memory
 	out[gid] = localArray[lid];
 }
-/*
-In diesem Fall kann die Kernel-Funktion Calculate vom Host-Code aus aufgerufen werden,
-die Funktion Sum dagegen nur innerhalb des Device-Codes.
-*/
